2b33eeebfa22a25fa890dc6eb58b99385a5fe982
oxymor0n <oxymor0n@outlook.com>
2016-07-12 15:09:39 -0400

    add info box with d3 graph, and overlay selection

--- render-controls.js
+++ render-controls.js
@@ -1,7 +1,7 @@
 var waitOnMapData = setInterval(function () {
   // create an object that holds all the GeoJSON map layers that we can switch between
   var mapLayers = {
-    "311": layer311
+    "311 Complaints": geoJsonLayer311
   };
   
   // make sure all GeoJSON layers have been initialized
@@ -10,14 +10,25 @@ var waitOnMapData = setInterval(function () {
   for (layer in mapLayers) {
     if (mapLayers[map] === null) { return false; }
   }
-
-  // add the first GeoJSON layer to the map
-  layer311.addTo(map);
   
   // All maps have been initialized, create a layers selector
   console.log("All data have loaded, creating control");
-  L.control.layers(mapLayers, null, {position: "topright", collapsed: false}).addTo(map);
+  L.control.layers(null, mapLayers, {position: "topright", collapsed: false}).addTo(map);
   
   // clear the interval after we've finished creating the Layers control
   clearInterval(waitOnMapData);
 }, 100);
+
+var infoBoxes = {
+  "311 Complaints": infoBox311
+}
+
+map.on('overlayadd', function (event) {
+  console.log(event);
+  infoBoxes[event.name].addTo(map);
+});
+
+map.on('overlayremove', function (event) {
+  console.log(event);
+  infoBoxes[event.name].removeFrom(map);
+});
